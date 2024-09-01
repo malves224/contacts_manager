3ead5f26d8e2d6aa0c0d05f98135f917fbec0ca7
Matheus Alves <malves224@gmail.com>
2024-08-31 23:21:33 -0300

    feat(LoginController)#reset_password

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,6 +8,14 @@ class LoginController < ApplicationController
     render status: :unauthorized, json: { errors: ['Login ou senha inválidos'] }
   end
 
+  def forgot_password
+    user = User.find_by_email(params_permit[:email])
+    return render status: :unprocessable_entity, json: { errors: ['Email não cadastrado']} if user.nil?
+
+    user.reset_password
+    render status: :ok
+  end
+  
   private
 
   def params_permit
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: { message: 'Já existe um usuário com esse email' }
+
+  def reset_password
+    password = SecureRandom.hex(6)
+    PasswordMailer.new.reset_password(self, password).deliver
+  end
 end
