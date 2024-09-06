0e0ab566760731ab37130a0211ab989b60980bee
Matheus Alves <malves224@gmail.com>
2024-09-06 16:53:06 -0300

    feat(user) dependency exclusion for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :contacts, dependent: :destroy
 
   validates :email, presence: true, uniqueness: { message: 'Já existe um usuário com esse email' }
 
